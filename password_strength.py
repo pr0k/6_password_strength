54bc1d098dc6807aaae0a54146550b98a7777b49
pr01k <pr01k@yandex.ru>
2018-03-12 21:43:12 +0300

    added changes

--- password_strength.py
+++ password_strength.py
@@ -35,92 +35,93 @@ def get_parser_args():
     return parser.parse_args()
 
 
+def check_pass_allow(password):
+    if re.match(
+        r'^[A-Za-z](?:[A-Za-z\d~!@#$%^&*_+=`|\\()}{[\]:;\"\'<>,.?/-]){3,}$',
+        password,
+    ):
+        return True
+    else:
+        return False
+
+
 def get_password_strength(password, blacklist, users_info):
-    pass_evaluation = {
+    password_strength = {
         'rating': 1,
         'detail': 'password is not in the black list or '
         'user\'s personal information list'
     }
-    if re.match(
-        r'[A-Za-z](?:[A-Za-z\d~!@#$%^&*_+=`|\\()}{[\]:;"\'<>,.?/-]){3,}',
-        password,
-    ):
-        positive_regex_list = [
-            r'[.\S]{8,}',
-            r'\d',
-            r'.*\d+.*\d+.*',
-            r'[A-Z]',
-            r'.*[A-Z]+.*[A-Z]+.*',
-            r'[a-z]',
-            r'.*[a-z]+.*[a-z]+.*',
-            r'[~!@#$%^&*_+=`|\\()}{[\]:;"\'<>,.?/-]',
-            r'.*[~!@#$%^&*_+=`|\\()}{[\]:;"\'<>,.?/-]+'
-            r'.*[~!@#$%^&*_+=`|\\()}{[\]:;"\'<>,.?/-]+.*',
-        ]
-        negative_regex_list = [
-            r'\d\d\W\d\d\W\d{4}',
-            r'[ABEKMHOPCTYX]\d{3}[ABEKMHOPCTYX]{2}(\d{2,3})?',
-        ]
-
-        for regex in positive_regex_list:
-            if re.search(regex, password):
-                pass_evaluation['rating'] += 1
-
-        for regex in negative_regex_list:
-            if re.search(regex, password, re.I):
-                pass_evaluation['rating'] -= 2
-
-        if blacklist:
-            pass_evaluation = check_matches_in_file(
-                blacklist,
-                password,
-                pass_evaluation,
-            )
-        if users_info:
-            pass_evaluation = check_matches_in_file(
-                users_info,
-                password,
-                pass_evaluation,
-            )
-        if pass_evaluation['rating'] < 0:
-            pass_evaluation['rating'] = 1
-        return pass_evaluation
-    else:
-        pass_evaluation['detail'] = (
-            'error: the password contains unsupported characters '
-            'or it\'s to short\n'
-            'for more details run this with -h key'
+    positive_regex_list = [
+        r'[.\S]{8,}',
+        r'\d',
+        r'.*\d+.*\d+.*',
+        r'[A-Z]',
+        r'.*[A-Z]+.*[A-Z]+.*',
+        r'[a-z]',
+        r'.*[a-z]+.*[a-z]+.*',
+        r'[~!@#$%^&*_+=`|\\()}{[\]:;\"\'<>,.?/-]',
+        r'.*[~!@#$%^&*_+=`|\\()}{[\]:;\"\'<>,.?/-]+'
+        r'.*[~!@#$%^&*_+=`|\\()}{[\]:;\"\'<>,.?/-]+.*',
+    ]
+    negative_regex_list = [
+        r'\d\d\W\d\d\W\d{4}',
+        r'[ABEKMHOPCTYX]\d{3}[ABEKMHOPCTYX]{2}(\d{2,3})?',
+    ]
+    for regex in positive_regex_list:
+        if re.search(regex, password):
+            password_strength['rating'] += 1
+    for regex in negative_regex_list:
+        if re.search(regex, password, re.I):
+            password_strength['rating'] -= 2
+    if blacklist:
+        password_strength = check_matches_in_file(
+            blacklist,
+            password,
+            password_strength,
         )
-        return pass_evaluation
+    if users_info:
+        password_strength = check_matches_in_file(
+            users_info,
+            password,
+            password_strength,
+        )
+    if password_strength['rating'] < 0:
+        password_strength['rating'] = 1
+    return password_strength
 
 
-def check_matches_in_file(file_to_check, password, pass_evaluation):
+def check_matches_in_file(file_to_check, password, password_strength):
     with open(file_to_check, 'r') as file_to_check:
         for pattern in file_to_check.read().split():
             if re.search(pattern, password):
-                pass_evaluation['rating'] -= 5
-                pass_evaluation['detail'] = (
-                    'warning: password or part of it '
-                    'was found in the <%s>!' % file_to_check.name
-                )
-    return pass_evaluation
+                password_strength['rating'] -= 5
+                password_strength['detail'] = 'warning: password or '\
+                    'part of it was found in the <%s>!' % file_to_check.name
+    return password_strength
 
 if __name__ == '__main__':
     try:
         args = get_parser_args()
-        password = str(input('Enter your password: '))
-        pass_evaluation = get_password_strength(
-            password,
-            args.blacklist,
-            args.users_info,
-        )
-        print(
-            '\nevalation of password %s\n' % password,
-            'rating is %s' % pass_evaluation['rating'],
-            pass_evaluation['detail'],
-            sep='\n',
-            end='\n\n',
-        )
+        password = input('Enter your password: ')
+        if check_pass_allow(password):
+            password_strength = get_password_strength(
+                password,
+                args.blacklist,
+                args.users_info,
+            )
+            print(
+                '\nstrength of password %s\n' % password,
+                'rating is %s' % password_strength['rating'],
+                password_strength['detail'],
+                sep='\n',
+                end='\n\n',
+            )
+        else:
+            print(
+                '\nerror: the password contains unsupported characters '
+                'or it\'s to short\n'
+                'for more details run this with -h key\n'
+            )
     except (ValueError, UnicodeDecodeError, TypeError):
         print(
             '\nerror:\tthere is no text in the file '
